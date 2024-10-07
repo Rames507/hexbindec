4c4d11840e7cbaa857c26a3bb916270dc92e700f
Rames507 <fragen.mobil@gmail.com>
2024-10-07 23:14:50 +0200

    Binary output will now be grouped into 4 bits; fixed minor oversights

--- src/main.rs
+++ src/main.rs
@@ -16,30 +16,58 @@ fn main() {
 
 fn get_number_from_user() -> Result<i64, ParseIntError> {
     let mut input = String::new();
-    println!("Please enter a number (0x/0b/0 ):");
+    println!("Please enter a number (0x/0b/0o):");
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
     input = input.trim().to_string();
 
-    parser_user_input(&input)
+    parse_user_input(&input)
 }
 
-fn parser_user_input(input: &String) -> Result<i64, ParseIntError> {
+fn parse_user_input(input: &String) -> Result<i64, ParseIntError> {
     let result = match &input[0..2] {
         "0x" => i64::from_str_radix(input.strip_prefix("0x").unwrap(), 16)?,
         "0b" => i64::from_str_radix(input.strip_prefix("0b").unwrap(), 2)?,
-        "0 " => i64::from_str_radix(input.strip_prefix("0 ").unwrap(), 8)?,
+        "0o" => i64::from_str_radix(input.strip_prefix("0 ").unwrap(), 8)?,
         _ => input.parse::<i64>()?,
     };
     Ok(result)
 }
 
+/// Returns a binary representation of a number with groups of 4 digits separated by a space
+///
+/// # Arguments
+///
+/// * `number`: The number to convert
+///
+/// returns: String
+///
+fn group_binary(number: i64) -> String {
+    let binary = format!("{:b}", number);
+    let mut grouped = String::new();
+
+    let padding = 4 - (binary.len() % 4);
+    if padding != 4 {
+        grouped.push_str(&"0".repeat(padding));
+    }
+    grouped.push_str(&binary);
+
+    let chunks: Vec<String> = grouped
+        .chars()
+        .collect::<Vec<char>>()
+        .chunks(4)
+        .map(|chunk| chunk.iter().collect())
+        .collect();
+
+    chunks.join(" ")
+}
+
 fn print_num(number: i64) {
     println!("Your number in different bases:");
     println!("Dec:\t  {}", number);
-    println!("Hex:\t0x{:X}", number);
-    println!("Bin:\t0b{:b}", number);
-    println!("Oct:\t0 {:o}", number);
+    println!("Hex:\t{:#X}", number);
+    println!("Bin:\t0b{}", group_binary(number));
+    println!("Oct:\t{:#o}", number);
     println!("{}", "-".repeat(20));
 }
